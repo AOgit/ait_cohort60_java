d6d86c7d887f981a6b75aabe7deb7f350890a004
Oleksandr <70403509@gmx.de>
2025-02-25 13:53:55 +0100

    homework_21 added

---
+++ src/homework_21/Student.java
@@ -0,0 +1,21 @@
+package homework_21;
+
+public class Student {
+    String name;
+    int age;
+    private int year; // курс в институте
+    double averageGrade;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setYear(int year){
+       this.year = year;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+}
