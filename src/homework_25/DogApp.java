80de442ff08b1db03fc558b18102aa87d6cf3c45
Oleksandr <70403509@gmx.de>
2025-03-03 18:04:22 +0100

    Homework for lesson 25

---
+++ src/homework_25/DogApp.java
@@ -0,0 +1,39 @@
+package homework_25;
+
+import java.util.Arrays;
+
+public class DogApp {
+    public static void main(String[] args) {
+
+       String[] dogNames = new String[]{"Пончо", "Леший", "Бургер", "Зефирка", "Чебурашка"};
+       Dog[] dogs = new Dog[dogNames.length];
+       // заполняем массив участниками
+       for (int i = 0; i < dogs.length; i++)  dogs[i] = new Dog(dogNames[i]);
+//        System.out.println(Arrays.toString(dogs))dd;
+
+        System.out.println("\nСоревнование началось!");
+        Dog.pause(1);
+        System.out.println("На старт! Внимание! Марш!");
+        Dog.pause(1);
+
+        for (int i = 0; i < Dog.barriers.length; i++) {
+            System.out.println("\n===================================================");
+            System.out.printf("Попытка участников взять %d-й барьер высотой %d см\n", i + 1, Dog.barriers[i]);
+            System.out.println("===================================================");
+            for (int j = 0; j < dogs.length; j++)
+            {
+                System.out.println();
+                System.out.printf("%d-й участник: \n", j + 1);
+                System.out.println(dogs[j]);
+                dogs[j].jumpBarrier(Dog.barriers[i]);
+            }
+        }
+
+        System.out.println("\n===РЕЗУЛЬТАТЫ СОРЕВНОВАНИЙ===");
+        for (int i = 0; i < dogs.length; i++) {
+            System.out.printf("Участник с именем %s преодолел %d барьерa/ов;\n", dogs[i].getName(), dogs[i].getPrizes());
+        }
+        System.out.println("=============================");
+
+    }
+}
---
+++ src/homework_25/Hw25Task2.java
@@ -0,0 +1,13 @@
+package homework_25;
+
+public class Hw25Task2 {
+    public static void main(String[] args) {
+//          ТASK 2
+          final int MAX_USERS = 100;
+//        MAX_USERS = 40;
+        // Ключевое слово final не позволит нам изменить значение переменной примитивного типа int
+
+        ImmutablePoint.printMessage("Hello, World");
+
+    }
+}
