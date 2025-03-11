b931d38de6590cfae39266e6027dd74fb9e3d451
Oleksandr <70403509@gmx.de>
2025-03-11 15:13:25 +0100

    homework 31 (generic MagicArray) added

--- src/_lists/MyArrayList.java
+++ src/_lists/MyArrayList.java
@@ -1,8 +1,8 @@
-/* package _lists;
+   package _lists;
 
 import java.util.Arrays;
 
-public class MyArrayList <T> {
+public class MyArrayList<T> implements MyList<T> {
     private T[] array;
     private int cursor;
 
@@ -15,16 +15,16 @@ public class MyArrayList <T> {
     }
 
     // Homework 22
-    public MyArrayList(int[] array) {
+    public MyArrayList(T[] array) {
 //        this.array = new int[array.length];
         if (array == null || array.length == 0) {
-            this.array = new int[10];
+            this.array = (T[]) new Object[10];
         } else {
 //            this.array = array;
 //            this.cursor = array.length;
 //            this.expandArray();
-            this.array = new int[array.length * 2]; // чтобы избежать expandArray
-            add(array);
+            this.array = (T[]) new Object[array.length * 2]; // чтобы избежать expandArray
+            addAll(array);
         }
     }
 
@@ -32,11 +32,11 @@ public class MyArrayList <T> {
         System.out.println(Arrays.toString(this.array));
     }
 
-    public void add(int value){
+    public void add(T object){
         if(this.cursor == this.array.length) {
             expandArray();
         }
-        this.array[this.cursor] = value;
+        this.array[this.cursor] = object;
         this.cursor++;
     }
 
@@ -45,7 +45,7 @@ public class MyArrayList <T> {
         // 2. Переписать в новый массив все значения из старого
         // 3. Перебросить указатель
         System.out.println("Расширяем внутренний массив! Курсор = " + this.cursor);
-        int[] newArray = new int[this.array.length * 2];
+        T[] newArray = (T[]) new Object[this.array.length * 2];
         for (int i = 0; i < cursor; i++){
             newArray[i] = array[i];
         }
@@ -56,19 +56,19 @@ public class MyArrayList <T> {
         return this.cursor;
    }
 
-    public int get(int index) {
+    public T get(int index) {
         // TODO Поправить обработку некорректного индекса
-        return this.isValidIndex(index) ?  this.array[index] : -2_147_483_647;
+        return this.isValidIndex(index) ?  this.array[index] : null;
    }
 
     // Добавление в массив нескольких элементов
-    public void add(int... numbers) {
+    public void addAll(T... objects) {
         // c numbers я могу обращаться точно также, как всо ссылкой на массив int
 //        System.out.println("Принял несколько int: " + numbers.length);
 //        System.out.println(Arrays.toString(numbers));
 
-       for (int i = 0; i < numbers.length; i++){
-           add(numbers[i]);
+       for (int i = 0; i < objects.length; i++){
+           add(objects[i]);
        }
 
     }
@@ -77,45 +77,54 @@ public class MyArrayList <T> {
         return index >= 0 && index < this.cursor;
     }
 
-    public int remove(int index) {
+    public T remove(int index) {
         if (this.isValidIndex(index)) {
-            int value = this.array[index];
+            T object = this.array[index];
 
             for (int i = index; i < this.cursor - 1; i++){
                this.array[i] = this.array[i + 1];
             }
 
             // Fixed Пофиксена проблема с последним элементом массива при удалении
-            this.array[this.size()-1] = 0;
+            this.array[this.size()-1] = null;
             this.cursor--;
-            return value;
+            return object;
         }
-        return -2_147_483_647; // здесь будет null
+        return null;
    }
 
    // Поиск по значению
-    public int indexOf(int value) {
+    public int indexOf(T object) {
         // Перебираю элементы
         // Если элемент равен искомому - вернуть индекс, иначе -1
         for(int i = 0; i < this.cursor; i++) {
-            if (this.array[i] == value) return i;
+            if (this.array[i].equals(object)) return i;
         }
         return -1;
     }
 
+    public boolean contains(T object) {
+        // Перебираю элементы
+        // Если элемент равен искомому - вернуть индекс, иначе -1
+        for(int i = 0; i < this.cursor; i++) {
+            if (this.array[i].equals(object)) return true;
+        }
+        return false;
+    }
+
     // Homework 22
-    public int lastIndexOf(int value){
+    public int lastIndexOf(T object){
         for(int i = cursor - 1; i >= 0; i--){
-            if(this.array[i] == value) return i;
+            if(this.array[i].equals(object)) return i;
         }
         return -1;
     }
 
     //  Homework 22
-    public boolean removeByValue(int value) {
-       int inx = indexOf(value);
+    public boolean remove(T object) {
+       int inx = indexOf(object);
        if (inx == -1) return false;
-       return remove(inx) == value;
+       return remove(inx).equals(object);
     }
 
     public String toString() {
@@ -128,12 +137,27 @@ public class MyArrayList <T> {
     }
 
     // TODO Написать метод public int[] toArray() возвращающий массив, состоящий из элементов магического массива.
-    public int[] toArray(){
-        int[] arr = new int[cursor];
+    public T[] toArray(){
+        T[] arr = (T[]) new Object[cursor];
         for (int i = 0; i < cursor; i++){
             arr[i] = this.array[i];
         }
         return arr;
     }
+
+    public boolean isEmpty() {
+        for (int i = 0; i < cursor; i++) {
+            if (this.array[i] != null) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void set(int index, T value) {
+        if (index > cursor - 1) {
+            System.out.println("Индекс выходит за предел массива. Элемент не может быть добавлен");
+            return;
+        }
+        this.array[index] = value;
+    }
 }
-*/
