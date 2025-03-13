4a3ab7b7a2c85d7ef4442412a8d4e80b0bab4756
Oleksandr <70403509@gmx.de>
2025-03-13 18:44:37 +0100

    homework 33 v1.1 added

--- src/lessons/lesson_33/persons/Person.java
+++ src/lessons/lesson_33/persons/Person.java
@@ -63,12 +63,13 @@ public class Person {
             if (Character.isDigit(ch)) passed[0] = true;
             if (Character.isLowerCase(ch)) passed[1] = true;
             if (Character.isUpperCase(ch)) passed[2] = true;
-            for(char specCh: "!%$@&*()[]<.-".toCharArray()) {
-                if (ch == specCh) {
-                    passed[3] = true;
-                    break;
-                }
-            }
+//            for(char specCh: "!%$@&*()[]<.-".toCharArray()) {
+//                if (ch == specCh) {
+//                    passed[3] = true;
+//                    break;
+//                }
+//            }
+        if ("!%$@&*()[]<.-".contains(String.valueOf(ch))) passed[3] = true;
         }
 //        System.out.println(Arrays.toString(passed));
         for (boolean bool: passed) if (!bool) return false;
