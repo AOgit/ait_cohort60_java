ad9337c57edbef6cc238a1341e8b5400949a55af
Oleksandr <70403509@gmx.de>
2025-03-17 18:52:24 +0100

    homework 35 added v1.3

--- src/homeworks/homework_35/RectangleUtils.java
+++ src/homeworks/homework_35/RectangleUtils.java
@@ -3,17 +3,26 @@ package homeworks.homework_35;
 import _lists.MyList;
 
 public class RectangleUtils {
+
     public static int removeDuplicates(MyList<Rectangle> myList, Rectangle rectangle) {
         int result = 0;
-        while (myList.contains((rectangle))){
+        while (myList.indexOf(rectangle) != myList.lastIndexOf(rectangle)){
             myList.remove(rectangle);
             result++;
         }
-        if (result > 0) {
-            myList.add(rectangle);
-            result--;
-        }
-
         return result;
     }
+//    public static int removeDuplicates(MyList<Rectangle> myList, Rectangle rectangle) {
+//        int result = 0;
+//        while (myList.contains((rectangle))){
+//            myList.remove(rectangle);
+//            result++;
+//        }
+//        if (result > 0) {
+//            myList.add(rectangle);
+//            result--;
+//        }
+//
+//        return result;
+//    }
 }
